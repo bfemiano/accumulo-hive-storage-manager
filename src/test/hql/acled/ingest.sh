cc50189deaacf8b89d83dec35086be6fdf3f9d17
Brian Femiano <bfemiano@new-host.home>
2013-04-20 17:12:42 -0400

    acled data is fetched during ingest. no longer checked in

--- src/test/hql/acled/ingest.sh
+++ src/test/hql/acled/ingest.sh
@@ -1,7 +1,23 @@
 pushd ingest
 hadoop fs -mkdir /input/acled
+if command -v wget 2>/dev/null; then
+	wget http://pficloud.s3.amazonaws.com/Nigeria_ACLED.csv
+elif command -v curl 2>/dev/null; then
+	curl -O http://pficloud.s3.amazonaws.com/Nigeria_ACLED.csv
+else 
+	echo "need to install curl or wget to fetch acled data"
+	exit 1;
+fi
 hadoop fs -put Nigeria_ACLED.csv /input/acled
 hadoop fs -mkdir /input/acled_cleaned
+if command -v wget 2>/dev/null; then
+	wget http://pficloud.s3.amazonaws.com/Nigeria_ACLED_cleaned.tsv
+elif command -v curl 2>/dev/null; then
+	curl -O http://pficloud.s3.amazonaws.com/Nigeria_ACLED_cleaned.tsv
+else 
+	echo "need to install curl or wget to fetch acled data"
+	exit 1;
+fi
 hadoop fs -put Nigeria_ACLED_cleaned.tsv /input/acled_cleaned
 hive -f load_acled.sql
 hive -f clean_and_transform_acled.sql
