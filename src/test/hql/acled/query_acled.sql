a4a38e7bc9319a5c285dda4093c62557d2c0994a
Brian Femiano <bfemiano@unknownb8f6b113542f.gateway.2wire.net>
2013-04-30 23:37:47 -0500

    added examples to query_acled.sql

--- src/test/hql/acled/query_acled.sql
+++ src/test/hql/acled/query_acled.sql
@@ -22,4 +22,24 @@ WITH SERDEPROPERTIES ('accumulo.columns.mapping' = 'rowID,cf|lat,cf|lon,cf|loc,c
 		
 select type,lat,lon from acled where pid = 3333 and fid = 20 and type like 'Violence%';
 
+select pid,fid from acled where pid > 3333 and fid < 30 limit 100;
+
+select lat,lon from acled where lat > 0.0 and lat < 10.0 and lon > 0.0 and lon < 10.0 limit 100;
+
+select * from acled where src = 'Reuters News';
+
+select * from acled where loc = 'Clough Creek';
+
+DROP TABLE IF EXISTS acled2;
+CREATE EXTERNAL TABLE acled2(rowid STRING, lat DOUBLE, lon DOUBLE, loc STRING, src STRING, type STRING) 
+STORED BY 'org.apache.accumulo.storagehandler.AccumuloStorageHandler' 
+WITH SERDEPROPERTIES ('accumulo.columns.mapping' = 'rowID,cf|lat,cf|lon,cf|loc,cf|src,cf|type',
+	'accumulo.no.iterators' = 'true',
+	'accumulo.table.name' = 'acled');
+
+select count(1) from acled2 where type = 'Violence against civilians';
+
+select a1.lat,a1.lon,a2.src from acled a1 JOIN acled2 a2 on a1.rowid = a2.rowid; 
+
+
 
